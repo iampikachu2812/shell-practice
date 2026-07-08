c763c1e4dcb20cc1b65546085039889fc17da8c5
iampikachu2812 <iampikachu2812@gmail.com>
2026-07-08 17:40:08 +0530

    variables

--- variables.sh
+++ variables.sh
@@ -2,6 +2,7 @@
 echo "All variables passed to script: $@"
 echo "Number of variables passed: $#"
 echo "First variable: $1"
+echo "second variable: $2"
 echo "Script name: $0"
 echo "Who is running this: $USER"
 echo "Which directory: $PWD"
