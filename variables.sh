24ea6ca3f98dab7f18cc040fd4461f3555059165
iampikachu2812 <iampikachu2812@gmail.com>
2026-07-08 18:09:19 +0530

    variables

--- variables.sh
+++ variables.sh
@@ -23,10 +23,11 @@ movies=("RRR" "Bahubali" "Salaar")
 #echo "new movies : ${movies[@]}"
 echo "best movie : ${movies[1]}"
 '
-Number =$1
+Number=$1
 if [ $Number -gt 20] ; then
 echo "Number is greater than 20"
 elif [ $Number -eq 20] ; then
 echo "Number is equal to 20"
 else
 echo "Number is less than 20"
+fi
